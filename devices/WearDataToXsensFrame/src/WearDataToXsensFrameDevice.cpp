fa61e55309df257043a6f692a8e8cea3410ef281
Claudia Latella <claudia.latella.86@gmail.com>
2018-07-10 16:08:44 +0200

    [WIP] Callback for the virtualLinkKinSensors

--- devices/WearDataToXsensFrame/src/WearDataToXsensFrameDevice.cpp
+++ devices/WearDataToXsensFrame/src/WearDataToXsensFrameDevice.cpp
@@ -6,10 +6,12 @@
  * GNU Lesser General Public License v2.1 or any later version.
  */
 
+#include "WearDataToXsensFrameDevice.h"
+
 #include <yarp/os/LogStream.h>
 #include <yarp/os/Network.h>
 
-#include "WearDataToXsensFrameDevice.h"
+#include <vector>
 
 using namespace yarp::dev;
 
@@ -22,6 +24,23 @@ const std::string logPrefix = DeviceName + " : ";
 
 bool WearDataToXsensFrameDevice::open(yarp::os::Searchable& config)
 {
+    // Default size for empty vectors
+    // ==============================
+    if (!config.check("linkVector_size")) {
+        yError() << logPrefix << "Size for the empty link-related variable not defined";
+        return false;
+    }
+    if (!config.check("sensorVector_size")) {
+        yError() << logPrefix << "Size for the empty sensor-related variable not defined";
+        return false;
+    }
+
+    // Get vector sizes from the config file
+    m_linkVectorSize = static_cast<unsigned>(config.find("linkVector_size").asInt());
+    m_sensorVectorSize = static_cast<unsigned>(config.find("sensorVector_size").asInt());
+
+    // Ports
+    // =====
     // Check ports in the config file
     if (!config.check("remotePort")) {
         yError() << logPrefix << "Remote port not found";
@@ -39,6 +58,7 @@ bool WearDataToXsensFrameDevice::open(yarp::os::Searchable& config)
         yError() << logPrefix << "Local output port2 not found";
         return false;
     }
+
     // Get the ports from the config file
     const std::string remotePort = config.find("remotePort").asString();
     const std::string localInputPort = config.find("input_localPort").asString();
@@ -82,3 +102,105 @@ bool WearDataToXsensFrameDevice::close()
     m_xsensSensorsPortToHDE.close();
     return true;
 }
+
+// ===================
+// TypedReaderCallback
+// ===================
+
+void WearDataToXsensFrameDevice::onRead(wearable::msg::WearData& inData)
+{
+    // inData contains fields as subdivided in the WearData.thrift
+
+    // Links Quantities
+    // ===================
+    // From ~WearData.thrift~
+    // --------------------------------------- //
+    //    struct VirtualLinkKinSensorData {    //
+    //      1: QuaternionWXYZ orientation,     //
+    //      2: VectorXYZ position,             //
+    //      3: VectorXYZ linearVelocity,       //
+    //      4: VectorXYZ angularVelocity,      //
+    //      5: VectorXYZ linearAcceleration,   //
+    //      6: VectorXYZ angularAcceleration   //
+    //    }                                    //
+    // --------------------------------------- //
+    //
+    // to ~XsensFrame.thrift~
+    // --------------------------------------- //
+    //    struct XsensSegmentData {            //
+    //        1:  Vector3 position;            //
+    //        2:  Vector3 velocity;            //
+    //        3:  Vector3 acceleration;        //
+    //        4:  Quaternion orientation;      //
+    //        5:  Vector3 angularVelocity;     //
+    //        6:  Vector3 angularAcceleration; //
+    //    }                                    //
+    // --------------------------------------- //
+
+    xsens::XsensSegmentsFrame xsSegmentFrame;
+
+    // Check if the VirtualLinkKinSensor is empty. If yes, replicate the same structure of the
+    // inData but with all the values equal to 0.0.
+    std::vector<wearable::msg::VirtualLinkKinSensor> input;
+
+    if (inData.virtualLinkKinSensors.empty()) {
+        // create a dummy wds element
+        wearable::msg::VirtualLinkKinSensor dummyElement_wds{
+            {"", // initialize name (from info field)
+             wearable::msg::SensorType::VIRTUAL_LINK_KIN_SENSOR, // initialize type (from info
+                                                                 // field)
+             wearable::msg::SensorStatus::ERROR}, // initialize status (from info field)
+            {{0.0, {0.0, 0.0, 0.0}}, // initialize quaternion (from data field)
+             {0.0, 0.0, 0.0}, // initialize position  (from data field)
+             {0.0, 0.0, 0.0}, // initialize lineraVelocity  (from data field)
+             {0.0, 0.0, 0.0}, // initialize angularVelocity  (from data field)
+             {0.0, 0.0, 0.0}, // initialize linearAcceleration  (from data field)
+             {0.0, 0.0, 0.0}}}; // initialize angularAcceleration  (from data field)
+
+        // create a dummy wds struct of m_linkVectorSize wds elements
+        std::vector<wearable::msg::VirtualLinkKinSensor> dummy_wds;
+        dummy_wds.resize(m_linkVectorSize, dummyElement_wds);
+
+        input = std::move(dummy_wds);
+    }
+    else {
+        input = std::move(inData.virtualLinkKinSensors);
+    }
+
+    xsSegmentFrame.segmentsData.reserve(input.size());
+    for (const auto& wds : input) {
+        xsens::XsensSegmentData xsSegmentData;
+
+        // TODO: check for each single field the possibility of a null vector.
+
+        // Quaternion
+        xsSegmentData.orientation.w = wds.data.orientation.w;
+        xsSegmentData.orientation.imaginary.x = wds.data.orientation.imaginary.x;
+        xsSegmentData.orientation.imaginary.y = wds.data.orientation.imaginary.y;
+        xsSegmentData.orientation.imaginary.z = wds.data.orientation.imaginary.z;
+        // Position
+        xsSegmentData.position.x = wds.data.position.x;
+        xsSegmentData.position.y = wds.data.position.y;
+        xsSegmentData.position.z = wds.data.position.z;
+        // Velocity
+        xsSegmentData.velocity.x = wds.data.linearVelocity.x;
+        xsSegmentData.velocity.y = wds.data.linearVelocity.y;
+        xsSegmentData.velocity.z = wds.data.linearVelocity.z;
+        // Acceleration
+        xsSegmentData.acceleration.x = wds.data.linearAcceleration.x;
+        xsSegmentData.acceleration.y = wds.data.linearAcceleration.y;
+        xsSegmentData.acceleration.z = wds.data.linearAcceleration.z;
+        // Angular Velocity
+        xsSegmentData.angularVelocity.x = wds.data.angularVelocity.x;
+        xsSegmentData.angularVelocity.y = wds.data.angularVelocity.y;
+        xsSegmentData.angularVelocity.z = wds.data.angularVelocity.z;
+        // Angular Acceleration
+        xsSegmentData.angularAcceleration.x = wds.data.angularAcceleration.x;
+        xsSegmentData.angularAcceleration.y = wds.data.angularAcceleration.y;
+        xsSegmentData.angularAcceleration.z = wds.data.angularAcceleration.z;
+
+        xsSegmentFrame.segmentsData.push_back(xsSegmentData);
+    }
+}
+// TODO: handle the status! From the ~WearData.thrift~, the STATUS exists per each sensor, in the
+// ~XsensFrame.thrift~ there is a STATUS for the whole Xsens suit.  Define a proper mapping.
